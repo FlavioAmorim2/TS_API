27eef9bd5537477fb1083bb121b71afcf84ab580
Flavio <flaviomatheus28013@gmail.com>
2024-12-17 20:40:07 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import fastify, {  FastifyInstance,  FastifyPluginOptions,  FastifyRequest,  FastifyReply,} from "fastify";
+import { request } from "http";
+
+export async function routes(
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions
+) {
+  fastify.get(
+    "/testing",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return { ok: true };
+    }
+  );
+
+  
+}
